1b6d27108688a62e79a5a7248ccb82bf6ffe4615
Dallin Penman <djpenman@gmail.com>
2016-09-20 00:41:20 -0600

    2670 assignments

---
+++ 2670 Assignments/Assets/IfStatements.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+
+    public float Apples = 5.5f;
+    public float oranges = 2.3f;
+    public float mangos = 3.3f;
+
+
+    // Update is called once per frame
+    void Start()
+    {
+
+        if (Apples < oranges)
+        {
+            print("There are more oranges");
+        }
+        if (oranges < Apples)
+        {
+            print("There are more apples");
+        }
+
+    }
+}
