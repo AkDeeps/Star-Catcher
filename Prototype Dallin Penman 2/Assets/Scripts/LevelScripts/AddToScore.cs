90c64162bfab3f3c48a32a136b69d5a333be18b3
Dallin Penman <djpenman@gmail.com>
2016-10-31 06:17:44 -0600

    updated UI text/ deleted things i dont need

---
+++ Prototype Dallin Penman 2/Assets/Scripts/LevelScripts/AddToScore.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AddToScore : MonoBehaviour {
+
+    public int pointsToAdd;
+    private int count = 0;
+    public Text countText;
+   
+
+    void Start()
+    {
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "star") 
+        {
+            count = count + 1;
+            SetCountText();
+            print(count);
+
+        }
+
+
+    }
+
+
+    void OnTriggerExit()
+    {
+        if (tag == "star") ;
+        Destroy(GameObject.FindWithTag("star"));
+    }
+
+
+
+    void SetCountText()
+    {
+        countText.text = "Score:  " + count.ToString();
+    }
+}
---
+++ Prototype Dallin Penman 2/Assets/Scripts/LevelScripts/CoinPickup.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CoinPickup : MonoBehaviour {
+	
+	public int pointsToAdd;
+    private int count = 0;
+    public Text countText;
+	
+
+    void Start()
+    {
+        SetCountText();
+    }
+
+	void OnTriggerEnter (Collider cother)
+	{
+
+        count = count + 1;
+        SetCountText();
+        print(count);
+
+
+	}
+	
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
