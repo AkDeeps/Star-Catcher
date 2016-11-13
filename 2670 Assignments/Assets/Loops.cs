a4f6c4a86be5f40019028eb4d4b7cdebdb274b88
Dallin Penman <djpenman@gmail.com>
2016-11-12 19:52:16 -0700

    random push

---
+++ 2670 Assignments/Assets/Loops.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+    public float Stuff = 10;
+
+	void ThingyMajigy()
+    {
+        for (int otherStuff = 5; otherStuff != Stuff; otherStuff++)
+        {
+            print(+otherStuff);
+        }
+        
+    }
+
+
+
+
+	}
