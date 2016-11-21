9affa0f3247a759c88c46be486fa8155abc8fe69
Dallin Penman <djpenman@gmail.com>
2016-11-21 08:28:58 -0700

    update ok

---
+++ Prototype Dallin Penman 2/Assets/Scripts/Overloading.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overloading : MonoBehaviour {
+
+
+
+
+    void Move (int speed)
+    {
+        Vector3 vector = new Vector3(speed, 0, 0);
+    }
+
+    void Move (string speed)
+    {
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Prototype Dallin Penman 2/Assets/Scripts/StaticVars.cs
+++ Prototype Dallin Penman 2/Assets/Scripts/StaticVars.cs
@@ -3,4 +3,6 @@ public class StaticVars {
 
     public static float distance = 10;
     public static float nextSectionPosition =20f;
+    public static float count = 0f;
+
 }
