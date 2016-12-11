8623ef50c729896b964e0bcb2a0a2282f0f5121d
Dallin Penman <djpenman@gmail.com>
2016-12-11 15:13:41 -0700

    k

---
+++ Anthony's Game/Assets/MonoBehaviors.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoBehaviors : MonoBehaviour {
+
+      void Awake()
+    {
+        print("hi how are ya");
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        Invoke("doSomething", 2);
+  
+       
+    }
+
+    void OnTriggerExit()
+    {
+        print("that was a weird feeling");
+    }
+
+    void doSomething()
+    {
+        print("whats happening");
+    }
+
+}   
+
---
+++ Prototype Dallin Penman 2/Assets/music.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class music : MonoBehaviour {
+
+
+    public bool playOnAwake;
+
+	
+	void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+}
