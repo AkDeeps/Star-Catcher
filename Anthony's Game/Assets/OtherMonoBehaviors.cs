39167bb1515c8f58e50f863aef59f993d95a3aba
Dallin Penman <djpenman@gmail.com>
2016-12-11 13:18:00 -0700

    anfrony's game

---
+++ Anthony's Game/Assets/OtherMonoBehaviors.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OtherMonoBehaviors : MonoBehaviour {
+
+  void Awake()
+    {
+        print("LETS START");
+    }
+
+	void OnMouseDown()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+
+  
+    
+}
