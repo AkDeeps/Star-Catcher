5d36960d60c6169350c042f28fe114a895acb544
Dallin Penman <djpenman@gmail.com>
2016-12-14 18:28:18 -0700

    updated

---
+++ Anthony's Game/Assets/Scripts/Lists.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Lists : MonoBehaviour {
+
+ void Start()
+    {
+        Dictionary<string,int> specialWeapons =
+            new Dictionary<string,int>();
+
+        specialWeapons.Add("Battle Axe", 1);
+        specialWeapons.Add("Battle Hammer", 1);
+        specialWeapons.Add("Battle Spear",1);
+
+
+
+
+        Dictionary<string, int> otherSpecialWeapons =
+            new Dictionary<string, int>();
+
+        otherSpecialWeapons.Add("Epic Axe", 1);
+        otherSpecialWeapons.Add("Epic Hammer", 1);
+        otherSpecialWeapons.Add("Epic Spear", 1);
+
+     
+    }
+}
---
+++ Anthony's Game/Assets/Scripts/Loops.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+
+    int differentTypesOfWeapons = 3;
+    int youHaveNoWeapon = 0;
+
+
+    void Start()
+    {
+        while (differentTypesOfWeapons > 1)
+        {
+            print("You have a weapon");
+            differentTypesOfWeapons--;
+        }
+
+        while (youHaveNoWeapon < 2)
+        {
+            print("You have no weapon");
+            youHaveNoWeapon++;
+        }
+
+        for(int myInt = 1; myInt < youHaveNoWeapon; myInt++)
+        {
+            print("adding weapons");
+        }
+
+        for(int myOtherInt = 1; myOtherInt > youHaveNoWeapon; myOtherInt--)
+        {
+            print("taking away weapon");
+        }
+
+        string[] myString = new string[3];
+
+        myString[0] = "Hammer";
+        myString[1] = "spear";
+        myString[2] = "Axe";
+
+        foreach(string item in myString)
+        {
+            print(item);
+        }
+
+        string[] myOtherString = new string[2];
+        myOtherString[0] = "Enemy wants you dead more now";
+        myOtherString[1] = "You better run kid";
+
+        foreach(string item in myOtherString)
+        {
+            print(item);
+        }
+    }
+}
